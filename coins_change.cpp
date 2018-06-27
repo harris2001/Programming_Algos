f360f803517e5fff76f0f84b5dda0844f1a3d469
hacker2001 <harrishadjiantonis@gmail.com>
2018-06-27 13:50:08 +0300

    Update coins_change.cpp

--- coins_change.cpp
+++ coins_change.cpp
@@ -1,21 +1,36 @@
 #include<iostream>
+#include<algorithm>
+#define INF 999999
 using namespace std;
-long long int dp[300][300];
+typedef long long int ll;
+
+ll dp[INF];
+
+void init(){
+    for(ll i=0; i<INF; i++){
+        dp[i][j]=0;
+    }
+}
+
 int main(){
-long long int poso,t;
-cin>>poso>>t;
-long long int pin[t];
+
+ll price,money,t;
+cin>>price>>money>>t;
+    
+ll pin[t],change=money-price;
+
 for(long long int i=0; i<t; i++){
     cin>>pin[i];
 }
-for(long long int i=0; i<t; i++){
-    for(long long int j=0; j<=poso; j++){
-        if(j>=pin[i]){dp[j]++;}
-        cout<<dp[j]<<" ";
+ sort(pin,pin+t);
+ init();
+for(ll i=0; i<t; i++){
+    for(ll j=0; dp[j]<=change-pin[i]; j++){
+        dp[dp[j]+pin[i]]++;
     }
     cout<<"\n";
 }
-    cout<<dp[poso]<<"\n";
+    cout<<"There are "<<dp[change]<<"ways of giving changes\n";
 
 return 0;
 }
