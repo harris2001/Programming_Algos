c3714e4fc80c49b4a367c7f86baab2824d120889
hacker2001 <harrishadjiantonis@gmail.com>
2021-03-02 19:09:15 +0200

    Commenting code to better explain it

--- CodeForces/1494/ABC String.cpp
+++ CodeForces/1494/ABC String.cpp
@@ -9,6 +9,7 @@ scanf("%d",&t);
 while(t--){
     string s;
     cin>>s;
+    //listing all the possible mappings of A,B, and C.
     char pin[8][3]={
         {'(','(','('},
         {'(','(',')'},
@@ -21,30 +22,33 @@ while(t--){
     };
 
     bool done=false;
+    //Try each of the above combinations
     for(int i=0; i<8; i++){
         int open=0,close=0;
         bool solve = true;
         string ss="";
         for(int j=0; j<s.size(); j++){
-            ss+=pin[i][s[j]-'A'];
+            //If the the current letter c(=s[j]) maps to an open parenthesis(pin[i][c-'A']), increase the number of open parenthesis
             if(pin[i][s[j]-'A']=='('){
                 open++;
             }
-            else{
+            else{   //Otherwise increase the number of closed parenthesis
                 close++;
             }
+            //If the number we have more closed parenthesis than open ones, or if the number of open parenthesis are more than half the number of characters, the combination is not valid -> break
             if(close>open || open>s.size()/2){
-                //cout<<ss<<endl;
                 solve=false;
                 break;
             }
         }
+        //If there above condition is not met, then the combination is valid -> Print YES
         if(solve==true){
             printf("YES\n");
             done=true;
             break;
         }
     }
+    //If there was no valid solution for any combination -> Print NO
     if(done==false){
         printf("NO\n");
     }
